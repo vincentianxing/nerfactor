2dc6361c484b0965278f9d0a1230b93a3b593a1e
12345Mandy <mandyhe@brown.edu>
2022-11-24 02:17:06 -0500

    supplement prev commit

--- install-blender-deps-MacOS.sh
+++ install-blender-deps-MacOS.sh
@@ -1,6 +1,6 @@
-cd ../software/blender-2.83.4-linux64/2.83/python/bin
+cd /Applications/Blender.app/Contents/Resources/3.3/python/bin
 # Install pip for THIS Blender-bundled Python
-curl https://bootstrap.pypa.io/get-pip.py | ./python3.7m
+curl https://bootstrap.pypa.io/get-pip.py | ./python3.10
 # If the above fails, make sure you deactivate your Conda environment
 # Use THIS pip to install other dependencies
 ./pip install absl-py tqdm ipython numpy Pillow opencv-python
