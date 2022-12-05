cleanup code, fix problematic gradient

nerfactor/models/nerfactor.py
@@ -261,10 +261,6 @@ class Model(ShapeModel):
         brdf = self._eval_brdf_at(
             surf2l, surf2c, normal_pred, albedo, brdf_prop) # NxLx3
         # ------ Rendering equation
-        
-        tf.print("lvis_pred")
-        tf.print(lvis_pred)
-
         rgb_pred, rgb_olat, rgb_probes = self._render( # all Nx3
             lvis_pred, brdf, surf2l, normal_pred, relight_olat=relight_olat,
             relight_probes=relight_probes)
@@ -326,22 +322,20 @@ class Model(ShapeModel):
             light = np.ones_like(self.light)
         if white_lvis_override:
             light_vis = np.ones_like(light_vis)
-        # cos = tf.einsum('ijk,ik->ij', l, n) # NxL
+        cos = tf.einsum('ijk,ik->ij', l, n) # NxL
         # Areas for intergration
         areas = tf.reshape(self.lareas, (1, -1, 1)) # 1xLx1
         # NOTE: unnecessary if light_vis already encodes it, but won't hurt
-        # front_lit = tf.cast(cos > 0, tf.float32)
-        lvis = light_vis # NxL
+        front_lit = tf.cast(cos > 0, tf.float32)
+        lvis = front_lit * light_vis # NxL
 
-        # tf.print("front_lit")
-        # tf.print(front_lit)
-        tf.print("lvis in render()")
-        tf.print(lvis)
+        # This is CRITICAL, clip 0 lvis to 1e-8!!!
+        lvis = tf.clip_by_value(lvis, 1e-8, 1.)
 
         def integrate(light):
             light_flat = tf.reshape(light, (-1, 3)) # Lx3
             light = lvis[:, :, None] * light_flat[None, :, :] # NxLx3
-            light_pix_contrib = brdf * light # NxLx3
+            light_pix_contrib = brdf * light * cos[:, :, None] # NxLx3
             rgb = tf.reduce_sum(light_pix_contrib, axis=1) # Nx3
             # Tonemapping
             rgb = tf.clip_by_value(rgb, 0., 1.) # NOTE
@@ -534,7 +528,7 @@ class Model(ShapeModel):
         if brdf_prop_jitter is not None:
             brdf_smooth_loss = smooth_loss(brdf_prop_pred, brdf_prop_jitter) # N
             loss += self.brdf_smooth_weight * brdf_smooth_loss
-        # Light should be smooth
+        # Light should NOT be smooth, because we are using point light
         # if mode == 'train':
         #     light = self.light
             # Spatial TV penalty

nerfactor/models/shape.py
@@ -127,62 +127,14 @@ class Model(BaseModel):
 
     def _calc_ldir(self, pts):  # light direction
 
-        toUse = self.lxyz
-
-        if (False):
-            print("Using surface sphere of lights")
-            envmap_h = 16
-            envmap_w = 32
-            envmap_radius = 100
-            lat_step_size = np.pi / (envmap_h + 2)
-            lng_step_size = 2 * np.pi / (envmap_w + 2)
-            lats = np.linspace(np.pi / 2 - lat_step_size, -np.pi / 2 + lat_step_size, envmap_h)
-
-            # print("Forcing only upper hemisphere")
-            # lats = np.linspace(np.pi / 2 - lat_step_size, 0, envmap_h)
-            print("Forcing radius 1.3")
-            envmap_radius = 1.3
-
-            lngs = np.linspace(np.pi - lng_step_size,     -np.pi + lng_step_size,     envmap_w)
-            lngs, lats = np.meshgrid(lngs, lats)
-            rlatlngs = np.dstack((envmap_radius * np.ones_like(lats), lats, lngs))
-            rlatlngs = rlatlngs.reshape(-1, 3)
-            lxyz = xm.geometry.sph.sph2cart(rlatlngs)
-
-            lxyz = lxyz.reshape(envmap_h, envmap_w, 3)
-
-            # print("Raising everything up by 100")
-            # lxyz = lxyz + np.array([0, 0, 100])[None, None, :]
-
-            toUse = tf.convert_to_tensor(lxyz, dtype=tf.float32)
-        else:
-            print("Using self.lxyz")
-
-        # print("Forcing origin 0")
-        surf2l = tf.reshape(toUse, (1, -1, 3)) - pts[:, None, :]
+        surf2l = tf.reshape(self.lxyz, (1, -1, 3)) - pts[:, None, :]
 
         tf.print("Max:")
         tf.print(tf.reduce_max(pts, axis=0))
         tf.print("Min:")
         tf.print(tf.reduce_min(pts, axis=0))
 
-        # tf.debugging.assert_less(
-        #     tf.linalg.norm(pts, axis=1), 10,
-        #     message="Found point further than 10 units from origin")
-
-        tf.debugging.assert_greater(
-            tf.linalg.norm(surf2l, axis=2), 0.001,
-            message="Found unusually low light displacements below 0.001")
-
-
         surf2l = mathutil.safe_l2_normalize(surf2l, axis=2)
-
-        # x = 1
-        # print("YOOYYOYOYOYOYOOY")
-        # print(pts.shape)
-        # tf.print(pts)
-        # if pts.shape[0] != None:
-        #     x = pts.shape[0]
 
         tf.debugging.assert_greater(
             tf.linalg.norm(surf2l, axis=2), 0.,